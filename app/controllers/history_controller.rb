89dfcdf64f6589c749dcd744b931e7791cb4f6e6
5hyn3 <hse9795@gmail.com>
2017-10-10 11:22:01 +0000

    add history write process

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,8 +1,14 @@
 class HistoryController < ApplicationController
   before_action :authenticate_user!
   def show
+    @histories = History.all
   end
 
   def add
+    history = History.new
+    history[:user_id] = current_user[:id]
+    history[:action] = 0
+    history.save
+    redirect_to :action => "show"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010104523) do
+ActiveRecord::Schema.define(version: 20171010105420) do
+
+  create_table "histories", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "action"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
